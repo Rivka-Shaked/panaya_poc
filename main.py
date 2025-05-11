975adb4c46c5707a7231d97bd3e1f5c7926cfa06
rivka.shaked <rivka.shaked@panaya.com>
2025-05-11 13:51:54 +0300

    fix agentName chain the timestamp

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ except json.JSONDecodeError as e:
 
 # Step 3: Append timestamp to agent_name
 if "agentName" in data:
-    timestamp = datetime.now().strftime("%d_%m_%y_%H_%M_%S_%f")[:-3]
+    timestamp = datetime.now().timestamp
     data["agentName"] = f'{data["agentName"]}_{timestamp}'
 
 # Step 4: Write fixed JSON to file
