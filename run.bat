972fad08cd0508a1d442d86bd5db6e3335b0170e
rivka.shaked <rivka.shaked@panaya.com>
2025-05-18 15:54:49 +0300

    s

--- run.bat
+++ run.bat
@@ -1,25 +1,3 @@
-@echo off
-setlocal
-
-REM Check if argument (JSON string) is provided
-if "%~1"=="" (
-    echo Usage: update-settings.bat "{json_string}"
-    exit /b 1
-)
-
-REM Store the JSON argument
-set "json=%~1"
-
-REM Set the path to the settings file
-set "settingsFile=C:\Users\ltuser.ghtestVM\AppData\Local\Agent\config\settings.json"
-
-REM Create the config folder if it doesn't exist
-if not exist "C:\Users\ltuser.ghtestVM\AppData\Local\Agent\config" (
-    mkdir "C:\Users\ltuser.ghtestVM\AppData\Local\Agent\config"
-)
-
-REM Use PowerShell to write the JSON to the file
-powershell -Command "Set-Content -Path '%settingsFile%' -Value '%json%' -Encoding UTF8"
 
 REM Start the Agent process
 start "" "C:\Program Files\Panaya\Agent\Agent.exe"
